99b95fa7392b90f00f695cc0ed15697416f28ab0
Eugene Otieno <otienoeo@gmail.com>
2023-01-16 21:00:45 +0300

    Added show list function

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,3 +4,25 @@ val taskList = mutableListOf<Task>()
 
 fun main() {
 }
+
+fun showList() {
+    for (i in taskList.indices) {
+        val task = taskList[i]
+        val num = i + 1
+        if (num <= 9) {
+            println("$num  ${task.date} ${task.time} ${task.priority} ${task.due}")
+            val str = task.tasks.split("\n")
+            for (s in str) {
+                println("   $s")
+            }
+            println()
+        } else {
+            println("$num ${task.date} ${task.time} ${task.priority} ${task.due}")
+            val str = task.tasks.split("\n")
+            for (s in str) {
+                println("   $s")
+            }
+            println()
+        }
+    }
+}
